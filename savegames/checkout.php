af22f19afed566cf8da82d534193e8f7b2bfcd93
Anton Johnsson Håkansson <anton.jh@hotmail.com>
2022-02-04 12:41:33 +0100

    Fixed null username in savegame checkout

--- savegames/checkout.php
+++ savegames/checkout.php
@@ -5,7 +5,7 @@ $filename = preg_replace("/[^a-zA-Z0-9_-]+/", "_", strtolower($_POST["name"]));
 $index = json_decode(file_get_contents("index.json"), true);
 $index[$filename] = [
     "checkedOut" => [
-        "username" => $_SERVER["PHP_AUTH_USER"],
+        "username" => $_SERVER["REMOTE_USER"],
         "timestamp" => time()
     ]
 ];
